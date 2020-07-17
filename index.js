d7210a1f8c5ca243efcd19dcd24279390972a183
Scott Kim <scott102938@gmail.com>
2020-07-17 18:41:28 +0900

    Initial commit for testing

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+
+const port = process.env.PORT || 5000;
+
+http.createServer(function(req, res){
+    console.log(req.url);
+    switch (req.url) {
+        case "/":
+            fs.readFile('index.html',function (err, data){
+                res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+                res.write(data);
+                res.end();
+            });
+            break
+        default:
+            fs.readFile('redirect.html',function (err, data){
+                res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+                res.write(data);
+                res.end();
+            });
+    }
+
+}).listen(port);
+
+console.log(`Server is running on port ${port}`);
