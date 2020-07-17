bd63ca88dc464c9857e138f2238401d8797c82f3
Scott Kim <scott102938@gmail.com>
2020-07-17 19:28:16 +0900

    Switch to express because I suspect a bug with my hardcoded http headers

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 5000;
 
-http.createServer(function(req, res){
-    console.log(req.url);
-    switch (req.url) {
-        case "/":
-            fs.readFile('index.html',function (err, data){
-                res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-                res.write(data);
-                res.end();
-            });
-            break
-        case "/RollingNews.png":
-            fs.readFile('RollingNews.png',function (err, data){
-                res.writeHead(200, {'Content-Type': 'image/png','Content-Length':data.length});
-                res.write(data);
-                res.end();
-            });
-            break
-        default:
-            fs.readFile('redirect.html',function (err, data){
-                res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-                res.write(data);
-                res.end();
-            });
-    }
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/RollingNews.png', (req, res) => res.sendFile(path.join(__dirname, 'RollingNews.png')));
+app.all('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './redirect.html'));
+});
 
-}).listen(port);
-
-console.log(`Server is running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`))
